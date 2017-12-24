6a11fca0d8286ee182862995ece5d178517bc363
Vlad Zadvorny <vladzadvorny@icloud.com>
2017-12-24 20:42:37 +0300

    create jwt auth and  team route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
 import React from 'react';
+import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
+import { ApolloLink } from 'apollo-link';
+import { setContext } from 'apollo-link-context';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
-import { ApolloProvider } from 'react-apollo';
 
 import Routes from './routes';
 
+const httpLink = createHttpLink({ uri: 'http://localhost:3001/graphql' });
+
+const middlewareLink = setContext(() => ({
+  headers: {
+    'x-token': localStorage.getItem('token'),
+    'x-refresh-token': localStorage.getItem('refreshToken')
+  }
+}));
+
+const afterwareLink = new ApolloLink((operation, forward) => {
+  const { headers } = operation.getContext();
+
+  if (headers) {
+    const token = headers.get('x-token');
+    const refreshToken = headers.get('x-refresh-token');
+
+    if (token) {
+      localStorage.setItem('token', token);
+    }
+
+    if (refreshToken) {
+      localStorage.setItem('refreshToken', refreshToken);
+    }
+  }
+
+  return forward(operation);
+});
+
+const link = afterwareLink.concat(middlewareLink.concat(httpLink));
+
 const client = new ApolloClient({
-  link: createHttpLink({ uri: 'http://localhost:3001/graphql' }),
+  link,
   cache: new InMemoryCache()
 });
 
