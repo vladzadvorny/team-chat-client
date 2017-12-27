e9b8c5d2119f050561198861a7949b4dcb9a2c43
Vlad Zadvorny <vladzadvorny@icloud.com>
2017-12-27 12:55:00 +0300

    start to refactor direcct messages

--- src/routes/DirectMessages.js
+++ src/routes/DirectMessages.js
@@ -12,9 +12,8 @@ import MessageContainer from '../containers/MessageContainer';
 import { meQuery } from '../graphql/team';
 
 const ViewTeam = ({
-  mutate,
   data: { loading, me },
-  match: { params: { teamId, channelId } }
+  match: { params: { teamId, userId } }
 }) => {
   if (loading) {
     return null;
@@ -30,13 +29,6 @@ const ViewTeam = ({
   const teamIdx = teamIdInteger ? findIndex(teams, ['id', teamIdInteger]) : 0;
   const team = teamIdx === -1 ? teams[0] : teams[teamIdx];
 
-  const channelIdInteger = parseInt(channelId, 10);
-  const channelIdx = channelIdInteger
-    ? findIndex(team.channels, ['id', channelIdInteger])
-    : 0;
-  const channel =
-    channelIdx === -1 ? team.channels[0] : team.channels[channelIdx];
-
   return (
     <AppLayout>
       <Sidebar
@@ -47,16 +39,9 @@ const ViewTeam = ({
         team={team}
         username={username}
       />
-      {channel && <Header channelName={channel.name} />}
-      {channel && <MessageContainer channelId={channel.id} />}
-      {channel && (
-        <SendMessage
-          placeholder={channel.name}
-          onSubmit={async text => {
-            await mutate({ variables: { text, channelId: channel.id } });
-          }}
-        />
-      )}
+      {/* <Header channelName={channel.name} />
+      <MessageContainer channelId={channel.id} /> */}
+      <SendMessage onSubmit={() => {}} placeholder={userId} />
     </AppLayout>
   );
 };
